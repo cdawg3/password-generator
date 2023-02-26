e5a4ae9b5d2240187079d951a31c3cc0344bf4ba
cdawg3 <cdawg4604@gmail.com>
2023-02-26 11:05:42 -0600

    password generator is functional

---
+++ index.js
@@ -0,0 +1,121 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password = []
+let displayOne = document.getElementById("display1")
+let displayTwo = document.getElementById("display2")
+
+function handleGenerate() {
+    displayOne.textContent = generatePassword()
+    displayTwo.textContent = generatePassword()
+}
+
+function generatePassword() {
+    password = [];
+    let character = ""
+    for (let i = 0; i < 16; i++) {
+        character = randomCharacter()
+        password.push(character)
+    }
+    let newPassword = ""
+    for (let i = 0; i < password.length; i++) {
+        newPassword += password[i]
+    }
+    return newPassword
+}
+
+function randomCharacter() {
+    let index = Math.floor(Math.random() * characters.length)
+    return characters[index]
+}
